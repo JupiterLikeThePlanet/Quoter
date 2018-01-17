e5be6e530ba4c2f6547e7b2db2fb351e31829379
Jupiter <jupiterbaudot@gmail.com>
2018-01-17 10:17:31 -0800

    Change google style sheets to link tags

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #heroku background assets
-  config.serve_static_assets = true
+  # config.serve_static_assets = true
+  config.serve_static_files = true
 
 end
